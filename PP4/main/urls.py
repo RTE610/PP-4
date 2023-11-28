6ec0f43b8ac23815d199f678a08cb50dc74d54c9
RTE610 <127295577+RTE610@users.noreply.github.com>
2023-11-28 20:27:58 +0200

    Added on main page text HTTPSResponse

--- PP4/main/urls.py
+++ PP4/main/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-path('',views.test)
+path('',views.test),
+    path('api/v1/hello-world14',views.Task)
 ]
--- PP4/main/views.py
+++ PP4/main/views.py
@@ -1,3 +1,6 @@
 from django.http import HttpResponse
 def test(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse("Test")
+def Task(request):
+    return HttpResponse("Hello World!14")
+    return HttpResponse(response_text, status=200)
